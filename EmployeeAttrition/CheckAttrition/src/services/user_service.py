3e37212eb9eb1ce9324584bb4078265c02467402
sahilomatic <70945198+sahilomatic@users.noreply.github.com>
2021-03-26 14:29:01 +0530

    Add files via upload

---
+++ EmployeeAttrition/CheckAttrition/src/services/user_service.py
@@ -0,0 +1,15 @@
+class UserService(object):
+    """
+    service function for user related business logic
+    """
+
+    def login_user(self):
+        """
+        TASKS: write the logic here for user login
+               authenticate user credentials as per your
+               schema and return the identifier user.
+
+               raise appropriate errors wherever necessary
+        """
+
+        pass
