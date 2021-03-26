b3846bf02e8afcad557a7762f875762593ede1f8
sahilomatic <70945198+sahilomatic@users.noreply.github.com>
2021-03-26 14:30:06 +0530

    Add files via upload

---
+++ EmployeeAttrition/CheckAttrition/models/user.py
@@ -0,0 +1,12 @@
+from mongoengine import Document
+
+
+class User(Document):
+    """
+    TASK: Create a model for user with minimalistic
+          information required for user authentication
+
+    HINT: Do not store password as is.
+    """
+
+    pass
